1ecaf5e539c1eff54e7bfaaf622051d80493b990
AlexKhlybov <aghlybov@gmail.com>
2021-10-22 15:31:25 +0300

    STEP_1 Фикс

--- models/database.py
+++ models/database.py
@@ -1,5 +1,4 @@
 import os
-from os.path import join
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -7,25 +6,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-from config import BASEDIR, TESTING
 
 
-def create_sqlite_url():
-    return "sqlite:///" + join(BASEDIR, 'test_db.db3')
+DB_NAME = os.getenv("POSTGRES_DB")                  
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-
-if TESTING:
-    SQLALCHEMY_DATABASE_URL = create_sqlite_url()
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-else:
-    DB_NAME = os.getenv("POSTGRES_DB")                  
-    DB_USER = os.getenv("POSTGRES_USER")
-    DB_PASS = os.getenv("POSTGRES_PASSWORD")
-    DB_HOST = os.getenv("POSTGRES_HOST")
-    DB_PORT = os.getenv("POSTGRES_PORT")
-    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
